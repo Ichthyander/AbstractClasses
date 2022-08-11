2a8eeb15f1bdf22e976c81aca2f4af0ee09530c9
Ichtyander <di_ignatov@mail.ru>
2022-08-11 22:51:50 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    abstract class Animal
+    {
+        string name;
+
+        public abstract string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public abstract string Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(name);
+            Say();
+        }
+    }
+}
